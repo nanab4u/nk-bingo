2ed8e12803dfad6e3762e5c6e55f04db4b32727d
Narayanan Karuppiah <karuppiah_narayanan@cat.com>
2026-01-14 12:31:58 +0530

    More Lint

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,10 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'require-await': 'error',
+    },
   },
 ])
